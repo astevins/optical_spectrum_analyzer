b42beb5e1e1f52763b2f9d27d112816b4953a8e8
astevins <arya.stevinson@alumni.ubc.ca>
2021-05-29 12:07:48 -0400

    Add & test server request for x lims

--- osa/services/server_requests.py
+++ osa/services/server_requests.py
@@ -11,6 +11,7 @@ from osa.exceptions.response_timeout import ResponseTimeout
 def get_trace() -> Dict[str, Union[float, str, list[float]]]:
     """
     Requests trace data from osa server.
+    Converts x_increment to nanometers and returns x_units as nm
     """
 
     print("Requesting trace.")
@@ -33,3 +34,28 @@ def get_trace() -> Dict[str, Union[float, str, list[float]]]:
                 'data': trace_res['ydata']}
     except KeyError:
         raise InvalidResponse("Missing data in response to TRACE request.")
+
+
+def get_x_lims() -> list[int]:
+    """
+    Requests x limits from OSA server
+    :return: List of two integers: [start, stop] x limits in nm
+    """
+
+    try:
+        res = requests.get(API_URL + 'LIM', timeout=1)
+    except requests.exceptions.Timeout:
+        raise ResponseTimeout("LIM request timed out.")
+
+    try:
+        return __parse_lim__(res.text)
+    except:
+        raise (InvalidResponse("Invalid response to LIM request."))
+
+
+def __parse_lim__(s: str) -> list[float]:
+    return eval(__remove_console_prefix__(s))
+
+
+def __remove_console_prefix__(s: str):
+    return s.replace('+READY>', '', 1)
--- tests/services/test_server_requests.py
+++ tests/services/test_server_requests.py
@@ -40,7 +40,6 @@ class GetTraceTests(unittest.TestCase):
             return
         except Exception as e:
             self.fail(f"Wrong error thrown for trace request timeout: {e.__class__.__name__}")
-            return
 
         self.fail("No error thrown for trace request timeout")
 
@@ -55,9 +54,50 @@ class GetTraceTests(unittest.TestCase):
             return
         except Exception as e:
             self.fail(f"Wrong error thrown for invalid trace response: {e.__class__.__name__}")
-            return
 
         self.fail("No error thrown for invalid trace response")
+
+
+class GetLimTests(unittest.TestCase):
+    @patch('requests.get')
+    def test_valid_response(self, mock_get):
+        mock_get.return_value.text = "+READY>[1515, 1580]"
+
+        try:
+            response = server_requests.get_x_lims()
+        except:
+            self.fail("Error thrown for valid request response.")
+
+        self.assertEqual(response, [1515, 1580])
+
+    @patch('requests.get')
+    def test_response_timeout(self, mock_get):
+        mock_get.side_effect = requests.exceptions.Timeout
+
+        try:
+            response = server_requests.get_x_lims()
+        except ResponseTimeout as e:
+            self.assertIsNotNone(str(e))
+            return
+        except Exception as e:
+            self.fail(f"Wrong error thrown for lim request timeout: {e.__class__.__name__}")
+
+        self.fail("No error thrown for lim request timeout")
+
+    @patch('requests.get')
+    def test_invalid_response(self, mock_get):
+        mock_get.return_value = 'S8ehEak32JE3'
+
+        try:
+            response = server_requests.get_x_lims()
+        except InvalidResponse as e:
+            self.assertIsNotNone(str(e))
+            return
+        except Exception as e:
+            self.fail(f"Wrong error thrown for invalid lim response: {e.__class__.__name__}")
+            return
+
+        self.fail("No error thrown for invalid lim response")
 
 
 if __name__ == "__main__":
