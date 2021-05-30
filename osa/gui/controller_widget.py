a24af3872b170c0bc3a911c7a6b398bebf652c06
astevins <arya.stevinson@alumni.ubc.ca>
2021-05-29 22:17:29 -0400

    Add x-axis wavelength/frequency selection to controller (nonfunctional)

--- osa/gui/controller_widget.py
+++ osa/gui/controller_widget.py
@@ -1,11 +1,13 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import pyqtSlot, pyqtSignal, QObject
+from PyQt5.QtWidgets import QGroupBox
 
 
 class Signals(QObject):
     start_clicked = pyqtSignal()
     stop_clicked = pyqtSignal()
     single_clicked = pyqtSignal()
+
 
 class Controller(QtWidgets.QWidget):
     """
@@ -18,7 +20,9 @@ class Controller(QtWidgets.QWidget):
         super().__init__(*args, **kwargs)
 
         # Components
-        self.layout = QtWidgets.QGridLayout()
+        self.layout = QtWidgets.QHBoxLayout()
+        self.buttons_layout = QtWidgets.QVBoxLayout()
+        self.radio_button_group = QtWidgets.QButtonGroup()
         self.start_stop_button = QtWidgets.QPushButton('Start')
         self.single_button = QtWidgets.QPushButton('Single')
 
@@ -30,17 +34,38 @@ class Controller(QtWidgets.QWidget):
 
     def init_ui(self):
         self.setLayout(self.layout)
+        self.init_radio_buttons()
         self.init_start_stop_button()
         self.init_single_button()
+        self.layout.addLayout(self.buttons_layout)
 
     def init_start_stop_button(self):
         self.start_stop_button.setCheckable(True)
         self.start_stop_button.toggled.connect(self.start_stop_clicked)
-        self.layout.addWidget(self.start_stop_button)
+        self.buttons_layout.addWidget(self.start_stop_button)
 
     def init_single_button(self):
         self.single_button.clicked.connect(self.single_clicked)
-        self.layout.addWidget(self.single_button)
+        self.buttons_layout.addWidget(self.single_button)
+
+    def init_radio_buttons(self):
+        wavelength_radio_button = QtWidgets.QRadioButton('Wavelength')
+        frequency_radio_button = QtWidgets.QRadioButton('Frequency')
+
+        # Create visual radio button group container
+        radio_button_container = QtWidgets.QGroupBox()
+        radio_button_container.setTitle('X Axis:')
+        self.layout.addWidget(radio_button_container)
+        radio_button_container_layout = QtWidgets.QVBoxLayout()
+        radio_button_container.setLayout(radio_button_container_layout)
+        radio_button_container_layout.addWidget(wavelength_radio_button)
+        radio_button_container_layout.addWidget(frequency_radio_button)
+
+        # Create abstract radio button group
+        self.radio_button_group.setExclusive(True)
+        self.radio_button_group.addButton(wavelength_radio_button, 1)
+        self.radio_button_group.addButton(frequency_radio_button, 2)
+        wavelength_radio_button.setChecked(True)
 
     @pyqtSlot(bool)
     def start_stop_clicked(self, checked):
@@ -55,5 +80,4 @@ class Controller(QtWidgets.QWidget):
 
     @pyqtSlot()
     def single_clicked(self):
-        print("Single clicked")
         self.signals.single_clicked.emit()
